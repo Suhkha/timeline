f5b7c26610f5cebe5c536e415b6dc71740fdad3e
Jessica Villa <jessica.villa@nurun.mx>
2017-05-04 17:19:28 -0500

    js of active content

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -13,4 +13,30 @@ $('.owl-carousel').owlCarousel({
             items:5
         }
     }
-})
+});
+
+
+$('.year--content').each(function(){
+	$(this).hide();
+
+	if($(this).attr('id') == 'year-1'){
+		$(this).show();
+	}
+});
+
+$('.timeline--year a').on('click', function(e){
+	e.preventDefault();
+
+	$('.timeline--year a').removeClass('active');
+	$(this).addClass('active');
+
+	var year = $(this).attr('data-year'); 
+
+	$('.year--content').each(function(){
+		$(this).hide();
+
+		if($(this).attr('id') == year){
+			$(this).show();
+		}
+	});
+});
